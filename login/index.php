d31a5bf7e825db37a51ebfff8f9a3db204679bd9
elbisgithub <40470952+elbisgithub@users.noreply.github.com>
2018-06-21 13:48:53 -0500

    Update and rename .gitignore to login/index.php

---
+++ login/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title> GameStop </title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="../css/style.css" rel="stylesheet" type="text/css" media="screen" />
+<script type="text/javascript" src="../jquery/jquery-1.3.1.min.js"></script>
+<script type="text/javascript" src="../jquery/FuncionesWEBJS.js"></script>
+
+</head>
+<body>
+<div id="wrapper">
+  <div id="logo">
+		<br><br>
+		<img src="../imagenes/k/Head.jpg" width="100%">
+		<?php include ("../menu/MenuLogin.php"); ?>
+	</div>
+	<!-- end #header -->
+	<!-- end #header-wrapper -->
+	<div id="page">	
+	<div id="page-bgtop">
+		<div id="content">
+        	<script>LlamarOpcion(2);</script>
+		</div>
+		<!-- end #content -->
+		
+		<div id="sidebar">
+					<ul>
+						<li><a><img src="../imagenes/k/T.jpg" width="100%" /></a></li>
+                        
+					</ul>
+               
+		</div>
+		
+		
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	</div>
+	<!-- end #page -->
+	<div id="footer">
+		<p>Ministerio del Poder Popular Para la Educación Universitaria<br/>
+Instito Universitario de Tecnología de Maracaibo<br/>
+Desarrollado por : Elbis Marmol </p>
+	</div>
+	<!-- end #footer -->
+</div>
+</body>
+</html>
